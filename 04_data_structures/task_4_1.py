cc70a632141022e0fee73fdc919928ce65f91177
realnakrul <na_krul@mail.ru>
2018-09-04 09:58:27 +0400

    task_4_1.py completed

---
+++ 04_data_structures/task_4_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Задание 4.1
+
+Обработать строку NAT таким образом,
+чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+NAT = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
+NAT=NAT.replace('Fast', 'Gigabit')
+print(NAT)
