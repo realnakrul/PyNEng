3646a0fb10d514acf9e1cb5ab29239ac2967e7ec
realnakrul <na_krul@mail.ru>
2018-09-09 15:33:24 +0400

    task_5_3a.py completed

--- 05_basic_scripts/task_5_3a.py
+++ 05_basic_scripts/task_5_3a.py
@@ -11,7 +11,7 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 То есть эту задачу можно решить без использования условия if и циклов for/while.
 '''
-
+import readline
 access_template = [
     'switchport mode access', 'switchport access vlan {}',
     'switchport nonegotiate', 'spanning-tree portfast',
@@ -22,3 +22,17 @@ trunk_template = [
     'switchport trunk encapsulation dot1q', 'switchport mode trunk',
     'switchport trunk allowed vlan {}'
 ]
+INT_DICT={
+'access': {
+'request': 'Enter VLAN number: ',
+'template': access_template
+}, 
+'trunk': {
+'request': 'Enter allowed VLANs: ',
+'template': trunk_template
+}}
+INT_MODE=input('Enter interface mode (access/trunk): ')
+INT_NUM=input('Enter interface type and number: ')
+VL=input(INT_DICT[INT_MODE]['request'])
+print('\ninterface '+INT_NUM)
+print('\n'.join(INT_DICT[INT_MODE]['template']).format(VL))
