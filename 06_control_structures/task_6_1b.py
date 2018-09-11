1bb358c98b56a254886e605f10abdf92c8e942f1
realnakrul <na_krul@mail.ru>
2018-09-11 14:12:34 +0400

    task_6_1b.py completed

--- 06_control_structures/task_6_1b.py
+++ 06_control_structures/task_6_1b.py
@@ -10,3 +10,30 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+import readline
+RESULT=''
+RANGE_COUNTER=0
+while not RESULT:
+    IP=input('Enter IP address: ').split('.')
+    if len(IP)==4:
+        #STR to INT conversion
+        for OCTET in range(len(IP)):
+            try:
+                IP[OCTET]=int(IP[OCTET])
+                if IP[OCTET]>=0 and IP[OCTET]<=255:
+                    RANGE_COUNTER+=1
+            except:
+                RANGE_COUNTER=0
+    if RANGE_COUNTER < 4:
+        print('Incorrect IPv4 address')
+    else:
+        RESULT='unused'
+if IP[0]==255 and IP[1]==255 and IP[2]==255 and IP[3]==255:
+    RESULT='local broadcast'
+elif IP[0]==0 and IP[1]==0 and IP[2]==0 and IP[3]==0:
+    RESULT='unassigned'
+elif IP[0]>=1 and IP[0]<=223:
+    RESULT='unicast'
+elif IP[0]>=224 and IP[0]<=239:
+    RESULT='multicast'
+print(RESULT)
