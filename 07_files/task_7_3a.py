1760ebb19c3cd594f0966d4ed431252acf4fdcab
realnakrul <na_krul@mail.ru>
2018-09-23 11:36:29 +0400

    task_7_3a.py completed

--- 07_files/task_7_3a.py
+++ 07_files/task_7_3a.py
@@ -12,3 +12,25 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+mega=[]
+sorted = False
+template=''' {}    {}   {}'''
+#reading from file
+with open('CAM_table.txt', 'r') as f:
+    for line in f:
+        content=line.rstrip().split()
+        if len(content)==4 and content[0] != '----':
+            mega.append(list((int(content[0]),content[1],content[3])))
+#sorting
+while not sorted:
+    flag=True
+    for i in range(len(mega)-1):
+        if mega[i][0] > mega[i+1][0]:
+            temp=mega[i]
+            mega[i]=mega[i+1]
+            mega[i+1]=temp
+            flag=False
+    sorted=flag
+#results output
+for i in range(len(mega)):
+    print(template.format(mega[i][0],mega[i][1],mega[i][2]))
