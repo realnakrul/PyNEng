e41656c56d7c2feddb68cb1d9a1f0462fd2350af
realnakrul <na_krul@mail.ru>
2018-09-23 11:40:52 +0400

    task_7_3b.py completed

--- 07_files/task_7_3b.py
+++ 07_files/task_7_3b.py
@@ -12,3 +12,11 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+import readline
+vlan=input('Enter VLAN number: ')
+template=''' {}    {}   {}'''
+with open('CAM_table.txt', 'r') as f:
+    for line in f:
+        content=line.rstrip().split()
+        if len(content)==4 and content[0] == vlan:
+            print(template.format(content[0],content[1], content[3]))
