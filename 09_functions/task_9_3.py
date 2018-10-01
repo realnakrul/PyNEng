04c6c2ca5c7070331746caabfe3d90bab7e5a67e
realnakrul <na_krul@mail.ru>
2018-10-01 14:09:36 +0400

    task_9_3.py completed

--- 09_functions/task_9_3.py
+++ 09_functions/task_9_3.py
@@ -22,3 +22,23 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+def get_int_vlan_map(conf):
+    result_access = {}
+    result_trunk = {}
+    int_start=False
+    with open(conf) as f:
+        for line in f:
+            if line.startswith('interface'):
+                int_name = line.split()[-1]
+                int_start=True
+            elif '!' in line:
+                int_start=False
+            elif int_start == True and 'access vlan' in line:
+                vlan=line.split()[-1]
+                result_access.update({int_name:vlan})
+            elif int_start == True and 'allowed vlan' in line:
+                vlan = line.split()[-1].split(',')
+                vlan = [int(vl_str) for vl_str in vlan]
+                result_trunk.update({int_name:vlan})
+    return result_access,result_trunk
+print(get_int_vlan_map('config_sw1.txt'))
