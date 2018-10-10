2aead71dcb8ce600cfd0d2422fc64f67a86d4a70
realnakrul <na_krul@mail.ru>
2018-10-10 12:11:35 +0400

    task_12_2.py completed

--- 12_useful_modules/task_12_2.py
+++ 12_useful_modules/task_12_2.py
@@ -30,3 +30,47 @@ IP-адреса могут быть в формате:
 
 Для выполнения задачи можно воспользоваться функцией check_ip_addresses из задания 12.1
 '''
+import subprocess
+import ipaddress
+
+iptocheck=['192.168.148.1', '192.168.148.10-192.168.148.11', '192.168.148.12-18', '192.168.148.128']
+
+def check_ip_addresses(iplist):
+    ipavailable=[]
+    ipbusy=[]
+    for i in iplist:
+        print('Trying',i,'...')
+        reply = subprocess.run(['ping', '-c', '3', i], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+        if reply.returncode == 0:
+            ipbusy.append(i)
+        else:
+            ipavailable.append(i)
+    return ipbusy, ipavailable
+
+def ipdecode(iplist):
+    ipdecoded=[]
+    decodefail=[]
+    for i in iplist:
+        try:
+            ipaddress.ip_address(i)
+            ipdecoded.append(i)
+        except ValueError:
+            decodefail.append(i)
+    if decodefail:
+        for i_fail in decodefail:
+            i_first,i_last=i_fail.split('-')
+            *base,i_first=i_first.split('.')
+            i_last=i_last.split('.')[-1]
+            i_res=''
+            for gen in range(int(i_first),int(i_last)+1):
+                try:
+                    i_res='.'.join([str(oct) for oct in base])+'.'+str(gen)
+                    ipaddress.ip_address(i_res)
+                    ipdecoded.append(i_res)
+                except ValueError:
+                    pass
+    return ipdecoded
+
+iptocheck=ipdecode(iptocheck)
+print(iptocheck)
+print(check_ip_addresses(iptocheck))
