4980f3c71a099860136983badb399785a83bd2e6
realnakrul <na_krul@mail.ru>
2018-10-25 16:20:38 +0400

    task_15_3a.py completed

--- 15_module_re/task_15_3a.py
+++ 15_module_re/task_15_3a.py
@@ -21,3 +21,25 @@
 диапазоны адресов и так далее, так как обрабатывается вывод команды, а не ввод пользователя.
 
 '''
+import readline
+import re
+from pprint import pprint
+f1=input('File:')
+if not f1:
+    f1='config_r1.txt'
+print(f1)
+
+def parse_cfg(f2):
+    result={}
+    with open(f2, 'r') as f:
+        for line in f:
+            match_intf = re.search('interface (\S*)',line)
+            if match_intf:
+                intf=match_intf.group(1)
+            match_ip = re.search('ip address '
+                              '(\d*\.\d*\.\d*\.\d*) '
+                              '(\d*\.\d*\.\d*\.\d*)',line)
+            if match_ip:
+                result.update({intf:match_ip.group(1,2)})
+    return result
+pprint(parse_cfg(f1))
