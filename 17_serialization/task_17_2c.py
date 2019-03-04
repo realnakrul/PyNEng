02158907ff55175f94980e8b8684b73544748189
realnakrul <na_krul@mail.ru>
2019-03-04 15:18:16 +0400

    task_17_2c.py completed

--- 17_serialization/task_17_2c.py
+++ 17_serialization/task_17_2c.py
@@ -30,3 +30,34 @@
 > pip install graphviz
 
 '''
+
+
+def neighbors_dedup(neighbors_dict):
+    """Removes duplicate connections from neighbors dictionary"""
+    neighbors_dict_dedup = {}
+    for n1 in neighbors_dict.keys():
+        duplicate_flag = False
+        for n2 in neighbors_dict_dedup.keys():
+            if (n1 == n2) or (n1 == neighbors_dict_dedup[n2]):
+                duplicate_flag = True
+        if not duplicate_flag:
+            neighbors_dict_dedup.update({n1: neighbors_dict[n1]})
+    return neighbors_dict_dedup
+
+
+if __name__ == '__main__':
+    import yaml
+    from pprint import pprint
+    from draw_network_graph import draw_topology
+    with open('topology.yaml') as f:
+        topology_old = yaml.load(f)
+    topology_new = {}
+    for k1 in topology_old.keys():
+        for k2 in topology_old[k1].keys():
+            local_dev = (k1, k2)
+            for k3 in topology_old[k1][k2].keys():
+                remote_dev = (k3, topology_old[k1][k2][k3])
+                topology_new.update({local_dev: remote_dev})
+    neighbors_dedup(topology_new)
+    draw_topology(neighbors_dedup(topology_new))
+    pprint(topology_new)
